1eaa2db5f7257208bb1fc1b5dd2f02d8d68642ec
thompsonma123 <51334278+thompsonma123@users.noreply.github.com>
2020-02-20 10:59:03 -0500

    .

---
+++ sandy/sandy/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  sandy
+//
+//  Created by Marshall Thompson on 2/20/20.
+//  Copyright © 2020 csci321. All rights reserved.
+//
+
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let TabViewController = "HomeVC"
+        
+    }
+    
+    
+}
+
