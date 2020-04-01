941837c1255a69fc02b3e5b8efd2525199add9be
Robert Manalo <robertmanalo@me.com>
2020-04-01 16:29:17 -0400

    Added Profile view
    This ProfileView will display your information and have the option to sign out.

---
+++ iosapp/iosSand/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  iosSand
+//
+//  Created by Robert Manalo on 4/1/20.
+//  Copyright © 2020 Madison Gipson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Sign Out")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
