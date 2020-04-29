fac5d4a6adc3edd8d54853af5a971beb7791da9f
Robert Manalo <robertmanalo@me.com>
2020-04-29 14:55:58 -0400

    Added Payment Buttons to PaymentView
    I added Venmo and Paypal buttons to Payment View. I also styled the Settings screen a bit.

--- iosapp/iosSand/Settings.swift
+++ iosapp/iosSand/Settings.swift
@@ -9,34 +9,64 @@
 import SwiftUI
 
 struct Settings: View {
+    init(){
+        UITableView.appearance().backgroundColor = .clear
+    }
     var body: some View {
         NavigationView {
             ZStack {
-                HStack {
                     VStack(alignment: .leading, spacing: 45, content: {
+                        Form {
                     NavigationLink(destination: ProfileView()) {
-                        Image(systemName: Constants.settingsNavigation.profileButton).font(.largeTitle)
+                        Image(systemName: Constants.settingsNavigation.profileButton).resizable()
+                            .frame(width:30, height:30)
+                            .padding()
+                            .foregroundColor(.gray)
                         Text("Profile")
+                            .font(.headline)
+                            .foregroundColor(Color.black.opacity(0.5))
+                            .padding()
                     }
                     NavigationLink(destination: PaymentView()) {
-                        Image(systemName: Constants.settingsNavigation.paymentButton).font(.largeTitle)
+                        Image(systemName: Constants.settingsNavigation.paymentButton).resizable()
+                            .frame(width:30, height:30)
+                            .padding()
+                            .foregroundColor(.gray)
                         Text("Payment")
+                            .font(.headline)
+                            .foregroundColor(Color.black.opacity(0.5))
+                            .padding()
                     }
                     NavigationLink(destination: AboutView()) {
-                        Image(systemName: Constants.settingsNavigation.aboutButton).font(.largeTitle)
+                        Image(systemName: Constants.settingsNavigation.aboutButton).resizable()
+                            .frame(width:30, height:30)
+                            .padding()
+                            .foregroundColor(.gray)
                         Text("About")
+                        .font(.headline)
+                        .foregroundColor(Color.black.opacity(0.5))
+                        .padding()
                     }
-                }).foregroundColor(Color.black.opacity(0.5))
-                    .navigationBarTitle("Settings")
-                    Spacer()
-                }.padding() 
-            }.frame(height: 175)
+                        }
+                        .background(Color(.white))
+                        .foregroundColor(Color.black.opacity(0.5))
+                })
+                        .navigationBarItems(leading:
+                        HStack {
+                        Text("Settings")
+                            .padding(200)
+                            .font(.system(size: 40, weight: .thin, design: .default))
+                            .foregroundColor(Color.yellow)
+                        })
+            }
+            }
+            
         }
     }
-}
 
 struct Settings_Previews: PreviewProvider {
     static var previews: some View {
         Settings()
     }
 }
+
